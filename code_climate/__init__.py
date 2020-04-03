af4a7f9d57f02faa2414f4b179c6478ff7be5138
João Daher <joao@daher.dev>
2020-04-03 14:59:13 -0300

    Add exceptions

--- code_climate/__init__.py
+++ code_climate/__init__.py
@@ -1,6 +1,13 @@
+from code_climate.exceptions import (
+    UnsupportedModelException,
+    TokenUndefinedException,
+)
+
 
 name = "code_climate"
 version = '0.0.1'
 
 __all__ = (
+    'TokenUndefinedException',
+    'UnsupportedModelException',
 )
